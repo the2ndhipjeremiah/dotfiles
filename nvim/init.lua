762eddcc2ca4d975839ee5d04d57257f22245c80
jeremy <the2ndhipjeremiah@gmail.com>
2025-12-18 21:36:29 +1300

    add clangd

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,6 +26,40 @@ vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
 
+--------------------------------------- PLUGS
+
+
+--------------------------------------- LSP
+-- Configure clangd
+vim.lsp.config('clangd', {
+  cmd = {'clangd', '--background-index', '--clang-tidy'},
+  root_markers = {'.git', 'compile_commands.json'},
+  filetypes = {'c'},
+})
+
+-- Enable for C files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {'c'},
+  callback = function()
+    vim.lsp.enable('clangd')
+  end,
+})
+
+-- Keybinds when LSP attaches
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local opts = {buffer = args.buf}
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+  end,
+})
 --------------------------------------- KEYMAPS
 local default_opts = {silent = true, noremap = true}
 vim.g.mapleader = " "
