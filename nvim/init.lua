c78f9afaa71a77241719724d55a735807a1dafd3
jeremy <the2ndhipjeremiah@gmail.com>
2026-02-01 13:31:26 +1300

    dap stuff, try to filter outputs

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,8 +27,11 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
 
 --------------------------------------- PLUGS
-
-
+vim.cmd([[
+  call plug#begin()
+  Plug 'mfussenegger/nvim-dap'
+  call plug#end()
+]])
 --------------------------------------- LSP
 -- Configure clangd
 vim.lsp.config('clangd', {
@@ -64,6 +67,28 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
   end,
 })
+--------------------------------------- DEBUG
+
+local dap = require("dap")
+dap.adapters.gdb = {
+  type = "executable",
+  command = "gdb",
+  args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+}
+
+dap.configurations.c = {
+  {
+    name = "Launch",
+    type = "gdb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+  },
+}
+
+local widgets = require('dap.ui.widgets')
 --------------------------------------- KEYMAPS
 local default_opts = {silent = true, noremap = true}
 vim.g.mapleader = " "
@@ -86,4 +111,73 @@ vim.keymap.set('n', '<leader>b', function()
     vim.opt.makeprg = './build.sh ' .. vim.fn.shellescape(file)
     vim.cmd('make!')
     vim.cmd('copen')
+end)
+
+-- convenience
+vim.keymap.set('i', '<C-s>', 'SDL_')
+-- Debug
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F6>', function() require('dap').terminate() end)
+
+vim.keymap.set('n', '<Left>', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Right>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<Down>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<Up>', function() require('dap').step_out() end)
+
+vim.keymap.set('n', '<F12>', function() require('dap').repl.toggle() end)
+
+--vim.keymap.set('n', '<leader>ds', function()
+--  local widgets = require('dap.ui.widgets')
+--  widgets.sidebar(widgets.scopes).open()
+--end)
+
+vim.keymap.set('n', '<leader>df', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.sidebar(widgets.frames).open()
+end)
+
+
+vim.keymap.set('n', '<leader>ds', function()
+  local widgets = require('dap.ui.widgets')
+  local sidebar = widgets.sidebar(widgets.scopes)
+  sidebar.open()
+  
+  vim.defer_fn(function()
+    local sidebar_buf = nil
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      local name = vim.api.nvim_buf_get_name(buf)
+      if name:match('dap%-scopes') or name:match('Scopes') then
+        sidebar_buf = buf
+        break
+      end
+    end
+    
+    if not sidebar_buf then return end
+    
+    vim.api.nvim_buf_set_option(sidebar_buf, 'modifiable', true)
+    
+    local lines = vim.api.nvim_buf_get_lines(sidebar_buf, 0, -1, false)
+    
+    local start_idx, end_idx
+    for i, line in ipairs(lines) do
+      if line == 'Locals' then
+        start_idx = i
+      elseif start_idx and line:match('^[A-Z]') then  -- next section (starts with capital, no indent)
+        end_idx = i - 1
+        break
+      end
+    end
+    
+    if start_idx then
+      end_idx = end_idx or #lines
+      local filtered = {}
+      for i = start_idx, end_idx do
+        table.insert(filtered, lines[i])
+      end
+      
+      vim.api.nvim_buf_set_lines(sidebar_buf, 0, -1, false, filtered)
+    end
+    
+    vim.api.nvim_buf_set_option(sidebar_buf, 'modifiable', false)
+  end, 200)
 end)
